161e8dbfeb7cb4cc0317a921dcbd8207db65554f
Region <672080538@qq.com>
2022-10-04 15:13:18 +0800

    把listview控件更换为listwidget控件 增加了列表显示文献名的功能 对namelist中的文献名作了修改（删除回车，空格替换为%20，方便搜索使用）

--- Exp_1/ui/bt_func.py
+++ Exp_1/ui/bt_func.py
@@ -1,10 +1,13 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QStringListModel
 from ui.Ref_bib_Download import Ui_Mainwindow
+import requests
 
 import re
 # PyMuPDF
 import fitz
 import pandas as pd
+
 
 # 这个类继承界面UI类
 class mywindow(QtWidgets.QWidget, Ui_Mainwindow):
@@ -19,44 +22,55 @@ class mywindow(QtWidgets.QWidget, Ui_Mainwindow):
         references_list = GetRefTxt(ref_list)
         L = len(references_list)
         name_list = []
+        # 将ref_list中元素逐个处理后加入name_list
         for i in range(L):
+            # 1.删除作者名和回车
+            if(references_list[i][0]!="["):
+                continue
             head = references_list[i].find('“')
-            tail = references_list[i].find('”',head+1)
-            papername = references_list[i][head+1:tail-1]
+            tail = references_list[i].find('”', head + 1)
+            papername = references_list[i][head + 1:tail - 1]
+            papername = papername.replace("\n", " ")
+            self.listWidget.addItem(references_list[i].replace("\n"," "))
+            # 2.把空格换成%20用来搜索
+            papername = papername.replace(" ", "%20")
             name_list.append(papername)
-        print(name_list)
+        #在listview中显示文献名称
+
+        # print(name_list)
+
 
 # 获取从references开始及之后的页面内容
 def GetRefPages(pdfname):
-    pdf=fitz.open(pdfname)
-    pagenum=len(pdf)
-    ref_list=[]
-    for num,p in enumerate(pdf):
-        content=p.get_text('blocks')
+    pdf = fitz.open(pdfname)
+    pagenum = len(pdf)
+    ref_list = []
+    for num, p in enumerate(pdf):
+        content = p.get_text('blocks')
         # print(num)
         for pc in content:
             # print(pc)
-            txtblocks=list(pc[4:-2])
-            txt=''.join(txtblocks)
+            txtblocks = list(pc[4:-2])
+            txt = ''.join(txtblocks)
             if 'References' in txt or 'REFERENCES' in txt or 'referenCes' in txt:
-                refpagenum=[i for i in range(num,pagenum)]
+                refpagenum = [i for i in range(num, pagenum)]
                 for rpn in refpagenum:
-                    refpage=pdf[rpn]
-                    refcontent=refpage.get_text('blocks')
-                    for n,refc in enumerate(refcontent):
-                        txtblocks=list(refc[4:-2])
+                    refpage = pdf[rpn]
+                    refcontent = refpage.get_text('blocks')
+                    for n, refc in enumerate(refcontent):
+                        txtblocks = list(refc[4:-2])
+                        # 将文献名转换成可供搜索的形式：把空格换成%20，去掉\n
                         ref_list.extend(txtblocks)
-    #print(''.join(ref_list))
+    # print(''.join(ref_list))
     return ref_list
+
 
 # 获取从references之后的文本内容
 def GetRefTxt(ref_list):
-    refnum=0
-    for nref,ref in enumerate(ref_list):
-        if 'References' in ref  or 'REFERENCES' in ref or 'referenCes' in ref:
-            refnum=nref
-    references_list=ref_list[refnum+1:]
-    #print(''.join(references_list))
+    refnum = 0
+    for nref, ref in enumerate(ref_list):
+        if 'References' in ref or 'REFERENCES' in ref or 'referenCes' in ref:
+            refnum = nref
+    references_list = ref_list[refnum + 1:]
+    # print(''.join(references_list))
     return references_list
-
-
