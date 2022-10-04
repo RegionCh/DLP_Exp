da826e5e103269c81e507f90d8e69e625ffda3ff
Region <672080538@qq.com>
2022-10-04 19:20:38 +0800

    大致完成了从DBPL爬取bib文件的功能！但有极小部分参考文献会爬取不到，原因见视频

--- Exp_1/ui/bt_func.py
+++ Exp_1/ui/bt_func.py
@@ -2,11 +2,8 @@ from PyQt5 import QtWidgets
 from PyQt5.QtCore import QStringListModel
 from ui.Ref_bib_Download import Ui_Mainwindow
 import requests
-
-import re
-# PyMuPDF
+import os
 import fitz
-import pandas as pd
 
 
 # 这个类继承界面UI类
@@ -17,7 +14,16 @@ class mywindow(QtWidgets.QWidget, Ui_Mainwindow):
 
     # 定义槽函数
     def Path(self):
+        # read the file and create the filedir
         FilePath = self.lineEdit.text()
+        b_loc = FilePath.rfind('\\')
+        e_loc = FilePath.find('.pdf')
+        TargetPaper = FilePath[b_loc + 1:e_loc].replace(":", "_")
+        now_loc = os.getcwd()
+        folder = now_loc + '\\' + TargetPaper
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+
         ref_list = GetRefPages(FilePath)
         references_list = GetRefTxt(ref_list)
         L = len(references_list)
@@ -25,19 +31,66 @@ class mywindow(QtWidgets.QWidget, Ui_Mainwindow):
         # 将ref_list中元素逐个处理后加入name_list
         for i in range(L):
             # 1.删除作者名和回车
-            if(references_list[i][0]!="["):
+            if (references_list[i][0] != "["):
                 continue
+            self.listWidget.addItem(references_list[i].replace("\n", " "))
+            # High fidelity video prediction with large stochastic recurrent neural networks
+            # High ﬁdelity video prediction with large stochastic recurrent neural networks
             head = references_list[i].find('“')
             tail = references_list[i].find('”', head + 1)
             papername = references_list[i][head + 1:tail - 1]
-            papername = papername.replace("\n", " ")
-            self.listWidget.addItem(references_list[i].replace("\n"," "))
+            # papername = papername.replace("-\n", "")
+            # papername = papername.replace("\n", " ")
+            papername = papername.replace("ﬁ", "fi")
+            papername = papername.replace("ﬂ", "fl")
+            papername = papername.replace("ﬃ", "ffi")
+            papername = papername.replace("ﬀ", "ff")
+
             # 2.把空格换成%20用来搜索
             papername = papername.replace(" ", "%20")
-            name_list.append(papername)
-        #在listview中显示文献名称
-
-        # print(name_list)
+            if papername.find("-\n") == -1:
+                name_list.append(papername.replace("\n"," "))
+            else:
+                papername1 = papername.replace("-\n", '-')
+                papername1 = papername1.replace("\n", ' ')
+                papername2 = papername.replace("-\n", '')
+                papername2 = papername2.replace("\n", ' ')
+                name_list.append(papername1)
+                name_list.append(papername2)
+        # download .bib file
+        # 1.get URL and download file
+        i = 1
+        j = 1
+        for papername in name_list:
+            search_url = "https://dblp.org/search?q=" + papername
+            r = requests.get(search_url)
+            # print(r.text)
+            b_loc = r.text.find("https://dblp.org/img/download.dark.hollow.16x16.png") - 120
+            b_loc = r.text.find("https", b_loc)
+            e_loc = r.text.find('"', b_loc)
+            bib_url = r.text[b_loc:e_loc]
+            # https://dblp.org/rec/journals/corr/abs-1812-00568.html?view=bibtex
+            # print(bib_url)
+            r1 = requests.get(bib_url)
+            b_loc = r1.text.find("download as .bib") - 90
+            b_loc = r1.text.find("https", b_loc)
+            e_loc = r1.text.find('"', b_loc)
+            download_url = r1.text[b_loc:e_loc]
+            if download_url == '':
+                print(str(j) + ".Not Found:  " + papername.replace("%20", " "))
+                j = j + 1
+                continue
+            # print(download_url)
+            file = requests.get(download_url, allow_redirects=True)
+            # 2.write the file to local
+            filename = folder + "\\" + papername.replace(":", "_")
+            filename = filename.replace("%20", " ")
+            with open(filename + ".bib", "wb") as f:
+                f.write(file.content)
+                print(str(i) + ".  " + filename.replace("%20", " "))
+                i = i + 1
+            f.close()
+        print("Done")
 
 
 # 获取从references开始及之后的页面内容
